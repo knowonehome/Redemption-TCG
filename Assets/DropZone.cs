bd755a8f1db9996d2f79113c993f3ade70971c6b
Kenny Peel <knowonehome@gmail.com>
2020-02-04 21:55:04 -0600

    finished demo

--- Assets/DropZone.cs
+++ Assets/DropZone.cs
@@ -7,17 +7,38 @@ public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoin
 {
     public void OnPointerEnter(PointerEventData eventData)
     {
+        if (eventData.pointerDrag == null)
+            return;
         //Debug.Log("OnPointerEnter");
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            d.placeholderParent = this.transform;
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        if (eventData.pointerDrag == null)
+            return;
         //Debug.Log("OnPointerExit");
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null && d.placeholderParent == this.transform)
+        {
+            d.placeholderParent = d.parentToReturnTo;
+        }
     }
 
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log(eventData.pointerDrag.name +"was dropped on " + gameObject.name);
-        eventData
+
+        //One way of making a card move to a different spot 
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if(d != null)
+        {
+            d.parentToReturnTo = this.transform;
+        }
+
     }
 }
